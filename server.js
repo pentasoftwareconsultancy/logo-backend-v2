a9c8e65ea1216047c0ae729f8ece35a7f920af14
Ankit Lal Sinha <ankitsinha25061909@gmail.com>
2025-06-23 16:55:51 +0530

    Connection with MongoDB database and server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+require('dotenv').config()
+
+
+const app = express();
+connectDB();
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is Running on PORT ${PORT}`);
+
+})
+
