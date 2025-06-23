b28b288b0abf537d48a778afec44f95ef6484d88
Ankit Lal Sinha <ankitsinha25061909@gmail.com>
2025-06-23 17:27:39 +0530

    User Login and Registration

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require('../models/UserModel')
+const bcrypt = require('bcryptjs')
+import generateToken from '../utils/generateToken.js';
+
+export const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ name, email, password: hashedPassword });
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+    });
+};
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+    res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+    });
+};  // eslint-disable-next-line
